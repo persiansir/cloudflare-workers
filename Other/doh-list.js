8c6418c7fd047096f9975e9be60a821d4640a65f
persiansir <48182760+persiansir@users.noreply.github.com>
2024-08-14 02:32:59 +0330

    Update doh-list.js

--- Other/doh-list.js
+++ Other/doh-list.js
@@ -1,15 +1,26 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+// ۱. یک شنونده رویداد به رویداد `fetch` اضافه می‌کنیم که هر زمان یک درخواست HTTP وارد شده توسط کارگر رخ می‌دهد، فعال می‌شود.
+addEventListener('fetch', async event => {
+  // ۲. درخواست وارد شده را مستقیماً به تابع `handleRequest` می‌دهیم.
+  event.respondWith(handleRequest(event.request));
 });
 
+// ۳. تابع `handleRequest` را تعریف می‌کنیم که یک شیء `request` را به عنوان آرگومان می‌گیرد.
 async function handleRequest(request) {
-  const response = await fetch('https://raw.githubusercontent.com/cslev/encrypted_dns_resolvers/main/doh_resolvers_data_20230510.json');
-  const data = await response.json();
+  // ۴. فایل JSON را از آدرس مشخص شده با استفاده از API `fetch` دریافت می‌کنیم و مستقیماً آن را تجزیه می‌کنیم.
+  const data = await (await fetch('https://raw.githubusercontent.com/cslev/encrypted_dns_resolvers/main/doh_resolvers_data_20230510.json')).json();
+  
+  // ۵. یک رشته خالی را برای ذخیره خروجی تعریف می‌کنیم.
   let output = '';
-  for (const [key, value] of Object.entries(data)) {
+  
+  // ۶. روی اشیاء در پاسخ با استفاده از `Object.values()` تکرار می‌کنیم که سریع‌تر از `Object.entries()` است.
+  for (const value of Object.values(data)) {
+    // ۷. ویژگی `uri` هر شیء را به رشته خروجی اضافه می‌کنیم، با جداکننده‌های کاراکتر جدید سطر (`\n`).
     output += `${value.uri}\n`;
   }
+  
+  // ۸. یک شیء پاسخ جدید با رشته خروجی به عنوان بدنه پاسخ ایجاد می‌کنیم.
   return new Response(output, {
+    // ۹. هدر `Content-Type` را به `text/plain` تنظیم می‌کنیم تا نشان دهیم که بدنه پاسخ متنی ساده است.
     headers: { 'Content-Type': 'text/plain' },
   });
 }
